4e2b2373bc728887fd2481385fe8a814573dfb92
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-11 16:21:04 +0200

    modification Controller.h - ajout attributs et methodes

--- preprojet/CV/Gauffre/Controller.h
+++ preprojet/CV/Gauffre/Controller.h
@@ -13,10 +13,33 @@ class Controller : public QMainWindow
 
 public:
     explicit Controller(QWidget *parent = 0);
+
     ~Controller();
 
 private:
+    initBoard(int w, int h);
+    changePlayer();
+    hasPlayed(Point p);
+    iaPlay();
+    isWon();
+
+private:
     Ui::Controller *ui;
+    int width, height;
+    board gameBoard;
+    mode gameMode;
+    bool turn;
 };
 
 #endif // CONTROLLER_H
+
+
+changeplayer(){
+    !turn;
+    if (mode == 1 && turn)
+        Timer(IAPlayed());
+    else if (mode == 2)
+        Timer(IAPlayed());
+    else if (!jeuFini)
+        joueurPlay();
+}
