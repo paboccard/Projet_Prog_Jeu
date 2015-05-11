c9bead3ccaa87a571aca46154e2421f1a700250e
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-11 17:29:19 +0200

    implementtation des methodes sauf iswon

--- preprojet/CV/Gauffre/Controller.h
+++ preprojet/CV/Gauffre/Controller.h
@@ -2,6 +2,10 @@
 #define CONTROLLER_H
 
 #include <QMainWindow>
+#include "../../Utils.h"
+#include "../../Automata/solver.h"
+
+
 
 namespace Ui {
 class Controller;
@@ -16,18 +20,21 @@ public:
 
     ~Controller();
 
+private slots:
+    void iaPlay();
+
 private:
-    initBoard(int w, int h);
-    changePlayer();
-    hasPlayed(Point p);
-    iaPlay();
-    isWon();
+    void initBoard(int w, int h);
+    void changePlayer();
+    void hasPlayed(Point p);
+    bool isWon();
 
 private:
     Ui::Controller *ui;
-    int width, height;
+    int width, height, delay;
     board gameBoard;
     mode gameMode;
+    difficulty gameDifficulty;
     bool turn;
 };
 
