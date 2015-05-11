68ab7ee244c33dd3c28f9761b28f0c663aa3e32a
lefrerej <lefrerej@F202-07.e-im2ag.ujf-grenoble.fr>
2015-05-11 15:58:39 +0200

    specification of Utils

--- preprojet/Utils.h
+++ preprojet/Utils.h
@@ -2,11 +2,23 @@
 #define UTILS_H
 #include <iostream>
 #include <vector>
+
+/*
+
+| | | | |
+| | | |0|
+
+0 is the point(1,3), x=1 and y=3.
+
+*/
 typedef struct{
     int x;
     int y;
 } Point;
 
-typedef std::vector<int> Board;
+/*
+forall i,j and i<j, board(j)<=board(i)
+ */
+typedef std::vector<int> board;
 
 #endif
