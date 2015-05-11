eaf0e60a9bd04ff2dfb9dcb9170230622d6a80c7
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-11 16:21:48 +0200

    implementation methode initBoard

--- preprojet/CV/Gauffre/Controller.cpp
+++ preprojet/CV/Gauffre/Controller.cpp
@@ -1,11 +1,16 @@
 #include "Controller.h"
 #include "ui_GameWindow.h"
+#include "../../Utils.h"
+#include <vector>
+
+using namespace std;
 
 Controller::Controller(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Controller)
 {
     ui->setupUi(this);
+
 }
 
 Controller::~Controller()
@@ -13,3 +18,20 @@ Controller::~Controller()
 
     delete ui;
 }
+
+
+void Controller::initBoard(int w, int h){
+    this->width = w;
+    this->height = h;
+    gameBoard.clear();
+    for (int i = 0; i < h; ++i) {
+        gameBoard.insert(w);
+    }
+    this->displayBoard();
+
+}
+
+//changePlayer();
+//hasPlayed(Point p);
+//iaPlay();
+//isWon();
