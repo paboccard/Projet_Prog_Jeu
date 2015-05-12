4f2c668cebe24a99a7cf51da1fc48c46d44271bf
jeremie finiel <jeremiefiniel@gmail.com>
2015-05-12 10:27:07 +0200

    Ajout d'accesseur

--- preprojet/CV/Gauffre/ConfigGameWindow.cpp
+++ preprojet/CV/Gauffre/ConfigGameWindow.cpp
@@ -12,3 +12,36 @@ ConfigGameWindow::~ConfigGameWindow()
 {
     delete ui;
 }
+
+int ConfigGameWindow::getWidth()
+{
+    return ui->widthSpinBox->value();
+}
+
+int ConfigGameWindow::getHeight()
+{
+    return ui->heightSpinBox->value();
+}
+
+mode ConfigGameWindow::getMode()
+{
+    if (ui->pvpRadioButton->isChecked())
+        return PvP;
+    else if (ui->pvcRadioButton->isChecked())
+        return PvC;
+    else
+        return CvC;
+}
+
+difficulty ConfigGameWindow::getDiff1()
+{
+    if (ui->pvcRadioButton->isChecked())
+        return (difficulty)ui->difficultypvcComboBox->currentIndex();
+    else
+        return (difficulty)ui->difficultycvcComboBox1->currentIndex();
+}
+
+difficulty ConfigGameWindow::getDiff2()
+{
+    return (difficulty)ui->difficultycvcComboBox2->currentIndex();
+}
