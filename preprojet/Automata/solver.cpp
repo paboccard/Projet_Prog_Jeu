f15c8ad0ee1154b8e0b6b70a80481138ca547fca
lefrerej <lefrerej@F202-07.e-im2ag.ujf-grenoble.fr>
2015-05-12 14:21:16 +0200

    add the first version of the third method for the Automata (Minimax)

--- preprojet/Automata/solver.cpp
+++ preprojet/Automata/solver.cpp
@@ -1,5 +1,7 @@
 #include "solver.h"
-
+#include <ctime>       
+#include <cstdlib>     
+#include <algorithm>
 using namespace std;
 
 int nbrLinesnotEmpty;
@@ -65,12 +67,60 @@ Point winnerLoser(board waffer){
     return p;
 }
 
+board eatWaffer(board waffer,int x, int y){
+    board waffertmp=waffer;
+    for(int k=x;k<waffertmp.size();k++)
+	if (waffertmp[k]>y)
+	    waffertmp[k]=y;
+    return waffertmp;
+}
+
+int Recursivminimax(board waffer,int currentIsPlayer1){
+    /* this boolean have a different signification according to currentIsPlayer1 :    
+       - currentIsPlayer1=false : loop until false 
+       - currentIsPlayer1=true  : loop until true 
+     */
+    int loop=1;
+    int res=!currentIsPlayer1;
+    if( (sumVector(waffer)==3 && waffer[0]==2) || sumVector(waffer)==1 )
+	return !currentIsPlayer1;
+    else{
+	for(int x=0;x<nbrLinesnotEmpty && loop;x++)
+	    for(int y=0;y<waffer[x] &&  loop;y++){
+		if(x || y){
+		    res=Recursivminimax(eatWaffer(waffer,x,y),0);
+		    if( (currentIsPlayer1 && res) || (!currentIsPlayer1 && !res) ) 
+			    loop=0;
+		}
+	    }
+    }
+    return currentIsPlayer1^loop;
+}
+
+
+
 Point minimax(board waffer){
     Point p;
-    p.x=0;
-    p.y=0;
-    return p; 
+    vector<Point> tab;
+    int isWinning=0,cptTab;
+    for(int x=0;x<nbrLinesnotEmpty;x++)
+	for(int y=0;y<waffer[x];y++){
+	    if(x || y){
+		tab[cptTab].x=x;
+		tab[cptTab].y=y;
+		cptTab++;
+	    }
+	}
+    srand ( unsigned ( std::time(0) ) );
+    random_shuffle ( tab.begin(), tab.end() );
+    for(int currentPoint=0;currentPoint<tab.size() && !isWinning;currentPoint++){
+	isWinning=Recursivminimax(eatWaffer(waffer,tab[currentPoint].x,tab[currentPoint].y),0);
+	p.x=tab[currentPoint].x;
+	p.y=tab[currentPoint].y;
+    }
+    return p;
 }
+
 
 Point play(board waffer, difficulty diff){
     Point p;
