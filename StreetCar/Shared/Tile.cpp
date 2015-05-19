ccbf8c224e5c156636ce30842028b42704b24ac2
lefrerej <lefrerej@F202-07.e-im2ag.ujf-grenoble.fr>
2015-05-19 17:13:38 +0200

    added the change function in the class Tile and remove a typological error in Utils.h

--- StreetCar/Shared/Tile.cpp
+++ StreetCar/Shared/Tile.cpp
@@ -4,7 +4,29 @@ public class Tile :public Square {
     bool tree;
     vector<Rail> ways;
     int turn;
-    idTile Type;
+    idTile type;
     int idPlayer;
     bool access [4];
+    
+
+    bool change(Tile t){
+	if(tree)
+	    return false;
+	switch (t.type){
+	case Straight:
+	    return type==Intersect || type==StraightLCurve || type==StraightRCurve || type==HStraightVCurve || type==VStraightVCurve || type==StraightLDoubleCurves || type==StraightRDoubleCurves;
+	case Curve:
+	    return !(type==Intersect);
+	case StraightRCurve:
+	    return type==StraightRDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
+	case StraightLCurve:
+	    return type==StraightLDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
+	case VCurve:
+	    return type==HStraightVCurve || type==VStraightVCurve || type==CrossCurves;
+	case DoubleCurves:
+	    return type==StraightLDoubleCurves || type==StraightRDoubleCurves || type==CrossCurves;
+	default:
+	    return false;
+	}
+    }
 }
