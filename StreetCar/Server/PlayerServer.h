b7522352dfba7bc296cd25a7df120584f2a210eb
Claude Goubet <claude.goubet@e.ujf-grenoble.fr>
2015-05-20 11:58:25 +0200

    restructuration du serveur, plus de classe Moteur, serveur deviens un main et nouvelle classe PlayerServer qui hérite de Player qui contiendra un proncond en plus.

---
+++ StreetCar/Server/PlayerServer.h
@@ -0,0 +1,16 @@
+#ifndef PLAYERSERVER_H
+#define PLAYERSERVER_H
+
+#include "../Shared/Player.h"
+
+
+class PlayerServer : public Player
+{
+    public:
+        PlayerServer();
+        ~PlayerServer();
+    private:
+
+};
+
+#endif // PLAYERSERVER_H
