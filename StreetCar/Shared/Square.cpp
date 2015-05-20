39e8accf156d39cbe2577d01ce22fa3cca8dc901
Jeremy Leyvraz <leyvraz.jrm@gmail.com>
2015-05-20 20:30:54 +0200

    ajout des types de square terminus et wall

--- StreetCar/Shared/Square.cpp
+++ StreetCar/Shared/Square.cpp
@@ -3,5 +3,17 @@
 Square::Square() {}
 
 bool Square::isEmpty(){
+	return type == idTile.Empty;
+}
+
+bool Square::isStation(){
+	return type > 11 && type < 20;
+}
+
+bool Square::isTerminus(){
 	return type == idTile.isEmpty;
+}
+
+bool Square::isWall(){
+	return type == idTile.Wall;
 }
