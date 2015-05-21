9c00887f1e882c71a9d535a6093dc45d0f86a865
Claude Goubet <claude.goubet@e.ujf-grenoble.fr>
2015-05-21 15:08:00 +0200

    pas important, écrasé par le pull...

--- StreetCar/Shared/Square.h
+++ StreetCar/Shared/Square.h
@@ -93,13 +93,5 @@ enum idTile {
     Empty=-1,
 };
 
-// enum of the differet types of packs in the protocol
-
-enum packs {
-    // CLIENT TO SERVER PACKS
-    STARTTRAVEL = 0, PLAYTRAVEL, STOPTRAVEL, PLAYTILE,
-    // SERVER TO CLIENT PACKS
-    PLAYEDTILE, PLAYEDTRAVEL, STARTEDTRAVEL, STOPPEDTRAVEL, VALIDATION, WON
-};
 
 #endif
