b569f2f70461d14153e49b46aec5ce2a6f822df1
jeremie finiel <jeremiefiniel@gmail.com>
2015-05-21 14:18:24 +0200

    Ajout de la classe Element

--- StreetCar/GUI/Element.h
+++ StreetCar/GUI/Element.h
@@ -2,15 +2,24 @@
 #define DEF_ELEMENT
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <
 
 class Element {
 	public:
-		Element() {};
-		void slot();
-		virtual SDL_Rect *getRect() = 0;
-		virtual void clicked() = 0;
-		virtual void mouseEnter() = 0;
+		Element(SDL_Surface *s, bool dde = false) {};
+		~Element();
+		void print(SDL_Renderer rend, int x, int y);
+		void setPosition(int x, int y);
+		bool isIn(int x, int y);
 
 	protected:
-		SDL_Rect *rect;	
-}
+		bool dragDropEnable;
+		bool mouseHover;
+		SDL_Rect rect;
+		SDL_Surface *surface;
+		
+		pthread_mutex_t mutexRect;
+};
+
+#endif
