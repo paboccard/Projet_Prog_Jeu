87bd8a7a0cfbaa394c9cc04f6493b273cd623c3b
Claude Goubet <claude.goubet@e.ujf-grenoble.fr>
2015-05-21 15:13:11 +0200

    suppr conflit

--- StreetCar/Shared/Tile.h
+++ StreetCar/Shared/Tile.h
@@ -1,11 +1,7 @@
 #ifndef TILE_H
 #define TILE_H
 #include "Square.h"
-<<<<<<< HEAD
-#include <iostream>
-=======
 #include "Utils.h"
->>>>>>> d5879a5788535e591473394e6154f14f448a363d
 #include <vector>
 #include <fstream>
 #define IMPOSSIBLE false
@@ -16,23 +12,18 @@
 #define EAST  2
 
 typedef struct {
-    int s1,s2; // s1 <= s2 . 
+    int s1,s2; // s1 <= s2 .
 } Rail;
 
-<<<<<<< HEAD
-class Tile:public Square {
- public:
-	 
-=======
+
 class Tile : public Square {
  public:
->>>>>>> d5879a5788535e591473394e6154f14f448a363d
     bool tree;
     std::vector<Rail> ways;
     int turn;
     idTile type;
     int idPlayer;
-    
+
     Tile();
 	Tile(idTile t, int p); // type is t and idPlayer is p
     bool change(Tile t);
