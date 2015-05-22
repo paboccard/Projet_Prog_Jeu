b28e3dd6a0071e0c766983c6ba868a44835a2589
jeremie finiel <jeremiefiniel@gmail.com>
2015-05-22 11:54:58 +0200

    ajout de la class game

---
+++ StreetCar/Shared/Game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+#include "Board.h"
+#include "Player.h"
+
+class Game {
+    int nbrPlayer;
+    vector<Player>players;
+    Board board;
+    int nbrSquare;
+
+	public:
+		Game();
+} 
+
+#endif
