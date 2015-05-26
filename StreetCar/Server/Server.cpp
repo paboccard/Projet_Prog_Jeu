76c7b914893001e7147eb3208612a286379b11f2
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-26 10:23:04 +0200

    add thread

--- StreetCar/Server/Server.cpp
+++ StreetCar/Server/Server.cpp
@@ -1,11 +1,15 @@
 #include "../Shared/Packs.h"
+#include "../Shared/Pack.h"
 #include "PlayerServer.h"
 #include <cstdlib>
+#include <pthread.h>
+#include "clientGuiHandler.h"
+
+using namespace std;
 
 int nbrPlayer;
 int currentPlayer;
 bool won = false;
-Board gameBoard;
 
 void thread(ProdCons<string> queueIn, ProdCons<string> queueOut){
 }
@@ -52,21 +56,65 @@ void tileplayed(Pack readPack){
 int main(int argc, char **argv){
 
     bool start = false;
-
+    Board gameBoard;
 
     vector<PlayerServer> players;
 
 
-    currentPlayer = rand() % nbrPlayer;
+    //currentPlayer = rand() % nbrPlayer;
 
 
-    while(!start){
+    //    while(!start){
     // TO-DO : initialization of the game
-        // must fill players with a vector of PlayerServer containing the list of Player + a ProdCons associated to him
+    // must fill players with a vector of PlayerServer containing the list of Player + a ProdCons associated to him
 
-        // wait for connexions, the first in is the host then new players for online game, else the gui for local games with all human players then the computers connect one by one
-        // when the host (online game) or the gui (local game) sends the message to start, set start to true and this is the end of the initialization.
-    }
+    // wait for connexions, the first in is the host then new players for online game, else the gui for local games with all human players then the computers connect one by one
+    // when the host (online game) or the gui (local game) sends the message to start, set start to true and this is the end of the initialization.
+    //gameBoard = Board();
+      
+    ProdCons<Pack> *prodConsOutputAutomate = new ProdCons<Pack>();
+    ProdCons<Pack> *prodConsOutputClientGui = new ProdCons<Pack>();
+    ProdCons<Pack> *prodConsCommon = new ProdCons<Pack>();
+
+    pthread_t clientGuiInput;
+    pthread_t clientGuiOutput;
+    pthread_t automateInput;
+    pthread_t automateOutput;
+
+    cout << endl;
+    if (pthread_create(&clientGuiInput, NULL, clientGuiInputHandler,(void *)(prodConsCommon))==0){
+	if (pthread_create(&clientGuiOutput, NULL, clientGuiOutputHandler,(void *)(prodConsOutputClientGui))==0){
+	    if (pthread_create(&automateInput, NULL, automateInputHandler,(void *)(prodConsCommon))==0){
+		if (pthread_create(&automateOutput, NULL, automateOutputHandler,(void *)(prodConsOutputAutomate))==0){
+		        
+		    pthread_join(automateOutput, NULL);
+		    cout << "End of event thread automateOutput" << endl;
+		}else
+		    cout << "ERROR, impossible to create automateOutput thread" << endl;
+		  
+		pthread_join(automateInput, NULL);
+		cout << "End of event thread automateInput" << endl;
+	    }else
+		cout << "ERROR, impossible to create automateInput thread" << endl;
+
+	    pthread_join(clientGuiOutput, NULL);
+	    cout << "End of event thread clientGuiOutput" << endl;
+	}else
+	    cout << "ERROR, impossible to create clientGuiOutput thread" << endl;
+	  
+	pthread_join(clientGuiInput, NULL);
+	cout << "End of event thread clientGuiInput" << endl;
+    }else
+	cout << "ERROR, impossible to create clientGuiInput thread" << endl;
+
+
+    delete prodConsCommon;
+    delete prodConsOutputAutomate;
+    delete prodConsOutputClientGui;
+
+
+
+    //}
 
     // here starts the referee
 
@@ -75,7 +123,7 @@ int main(int argc, char **argv){
     int readPlayer;
 
 
-    while(!won){
+    /*while(!won){
 
         readPack = players[currentPlayer].circularQueue.consume();
         idPack << readPack;
@@ -98,6 +146,6 @@ int main(int argc, char **argv){
         }
 
 
-    }
+	}*/
 
 }
