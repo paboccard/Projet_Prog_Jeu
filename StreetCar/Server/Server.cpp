6f027acb52e4f4383cbd6342182bd1e655bf9966
Claude Goubet <claude.goubet@e.ujf-grenoble.fr>
2015-05-26 10:52:59 +0200

    pull repare

--- StreetCar/Server/Server.cpp
+++ StreetCar/Server/Server.cpp
@@ -1,4 +1,5 @@
 #include "../Shared/Packs.h"
+#include "../Shared/Pack.h"
 #include "../Shared/Board.h"
 #include "../Shared/StartTravel.h"
 #include "../Shared/PlayTravel.h"
@@ -9,13 +10,16 @@
 #include "../Shared/PileTarget.h"
 #include "../Shared/Card.h"
 //#include "../Shared/PileWhenTravel.h"
+
 #include "PlayerServer.h"
 #include <cstdlib>
+#include <pthread.h>
+#include "clientGuiHandler.h"
 
-#define NB_TILE_MAX 2
 using namespace std;
 
 
+#define NB_TILE_MAX 2
 
 // sends an error pack to the specified error with the error descriptor
 void sendError(int player, error_pack error){
@@ -82,32 +86,66 @@ void tileplayed(PlayTile *readPack, int currentPlayer, Board gameBoard, vector<P
 }
 
 int main(int argc, char **argv){
-
-    bool start = false;
-
-
-    vector<PlayerServer> players;
-
-
-
-int main(int argc, char **argv){
     int nbrPlayer;
     int currentPlayer;
     int lastTravelLength = 0;
     bool start = false;
     bool won = false;
     vector<PlayerServer> players;
+    Pile pile = Pile();
 
-    currentPlayer = rand() % nbrPlayer;
+    //currentPlayer = rand() % nbrPlayer;
 
 
-    while(!start){
+    //    while(!start){
     // TO-DO : initialization of the game
-        // must fill players with a vector of PlayerServer containing the list of Player + a ProdCons associated to him
+    // must fill players with a vector of PlayerServer containing the list of Player + a ProdCons associated to him
 
-        // wait for connexions, the first in is the host then new players for online game, else the gui for local games with all human players then the computers connect one by one
-        // when the host (online game) or the gui (local game) sends the message to start, set start to true and this is the end of the initialization.
-    }
+    // wait for connexions, the first in is the host then new players for online game, else the gui for local games with all human players then the computers connect one by one
+    // when the host (online game) or the gui (local game) sends the message to start, set start to true and this is the end of the initialization.
+    //gameBoard = Board();
+
+    ProdCons<Pack> *prodConsOutputAutomate = new ProdCons<Pack>();
+    ProdCons<Pack> *prodConsOutputClientGui = new ProdCons<Pack>();
+    ProdCons<Pack> *prodConsCommon = new ProdCons<Pack>();
+
+    pthread_t clientGuiInput;
+    pthread_t clientGuiOutput;
+    pthread_t automateInput;
+    pthread_t automateOutput;
+
+    cout << endl;
+    if (pthread_create(&clientGuiInput, NULL, clientGuiInputHandler,(void *)(prodConsCommon))==0){
+        if (pthread_create(&clientGuiOutput, NULL, clientGuiOutputHandler,(void *)(prodConsOutputClientGui))==0){
+            if (pthread_create(&automateInput, NULL, automateInputHandler,(void *)(prodConsCommon))==0){
+                if (pthread_create(&automateOutput, NULL, automateOutputHandler,(void *)(prodConsOutputAutomate))==0){
+
+                    pthread_join(automateOutput, NULL);
+                    cout << "End of event thread automateOutput" << endl;
+                }else
+                    cout << "ERROR, impossible to create automateOutput thread" << endl;
+
+                pthread_join(automateInput, NULL);
+                cout << "End of event thread automateInput" << endl;
+            }else
+                cout << "ERROR, impossible to create automateInput thread" << endl;
+
+            pthread_join(clientGuiOutput, NULL);
+            cout << "End of event thread clientGuiOutput" << endl;
+        }else
+            cout << "ERROR, impossible to create clientGuiOutput thread" << endl;
+
+        pthread_join(clientGuiInput, NULL);
+        cout << "End of event thread clientGuiInput" << endl;
+    }else
+        cout << "ERROR, impossible to create clientGuiInput thread" << endl;
+
+
+    delete prodConsCommon;
+    delete prodConsOutputAutomate;
+    delete prodConsOutputClientGui;
+
+
 
     ///////////////////////////////
     // Game initialisation
@@ -146,34 +184,25 @@ int main(int argc, char **argv){
 
     while(!won){
 
-        readPack = players[currentPlayer].circularQueue.consume();
-        idPack << readPack;
-        readPlayer << readPack;
-        // if the pack was sent by the current player we call the appropriate function to validate or not the move, else we do nothing and wait for the write player to communicate.
         switch (readPack.idPack) {
-            case STARTTRAVEL :
-                travelstarted((StartTravel*)&readPack, currentPlayer, gameBoard);
-                break;
-            case PLAYTRAVEL :
-                travelplayed((PlayTravel*)&readPack, currentPlayer, gameBoard);
-                break;
-            case STOPTRAVEL :
-                travelstopped((StopTravel*)&readPack, currentPlayer, gameBoard);
-                break;
-            case PLAYTILE :
-                tileplayed((PlayTile*)&readPack, currentPlayer, gameBoard, players);
-                break;
-            case PILEWHENTRAVEL :
-                pilewhentravel((PileWhenTravel*)&readPack, currentPlayer, gameBoard);
-                break;
-            default :   //error, we do nothing
-                break;
-            }
-            readPlayer << readPack;
-
+        case STARTTRAVEL :
+            travelstarted((StartTravel*)&readPack, currentPlayer, gameBoard);
+            break;
+        case PLAYTRAVEL :
+            travelplayed((PlayTravel*)&readPack, currentPlayer, gameBoard);
+            break;
+        case STOPTRAVEL :
+            travelstopped((StopTravel*)&readPack, currentPlayer, gameBoard);
+            break;
+        case PLAYTILE :
+            tileplayed((PlayTile*)&readPack, currentPlayer, gameBoard, players);
+            break;
+        case PILEWHENTRAVEL :
+            pilewhentravel((PileWhenTravel*)&readPack, currentPlayer, gameBoard);
+            break;
+        default : //error, we do nothing
+            break;
         }
 
-
     }
-
 }
