dd4abedc230db097bab2616c577b0f5067dd19ee
lefrerej <lefrerej@F202-08.e-im2ag.ujf-grenoble.fr>
2015-05-26 14:58:34 +0200

    nouveau fichier: Computer.cpp 	nouveau fichier: Computer.h

---
+++ StreetCar/Computer/Computer.h
@@ -0,0 +1,22 @@
+#ifndef COMPUTER_H
+#define COMPUTER_H
+#define INFORMATIONS players[whoAmI]
+#include <vector>
+#include "../Shared/Player.h"
+#include "../Shared/Tile.h"
+#include "../Shared/Point.h"
+#include "../Shared/Board.h"
+#include "../Shared/Pile.h"
+class Computer {
+    Board board,boardTmp;
+    Point myTerminus[2][2];
+    std::vector<Player> players;
+    Pile pile;
+    int whoAmI,currentPlayer;
+    std::vector<Tile> path;
+    vector<Tile> createPath();
+ public:
+    Computer(std::vector<Player> allPlayers,int whoAmI);
+};
+
+#endif
