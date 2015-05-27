55563890487a03a6870b9afa4363766d90c3d8c0
jeremie finiel <jeremiefiniel@gmail.com>
2015-05-27 22:57:39 +0200

    indentation

--- StreetCar/Shared/Debug.h
+++ StreetCar/Shared/Debug.h
@@ -8,27 +8,27 @@
 #include <string>
 
 class Debug : public Pack{
-    
- public:
-    
-    std::string debug;
-    
-    Debug();
-    Debug(std::string deb);
 
-    void print(std::ostream& f);
-    void read(std::istream& f);
+	public:
 
-    friend std::ostream& operator << (std::ostream &f, Debug &t){
-	t.print(f);
-	return f;
-    };
-    friend std::istream& operator >> (std::istream &f, Debug &t){
-	std::cout << "JE MARCHE" << std::endl;
-	t.read(f);
-	std::cout << "JE MARCHE ENCORE" << std::endl;
-	return f;
-    };
+		std::string debug;
+
+		Debug();
+		Debug(std::string deb);
+
+		void print(std::ostream& f);
+		void read(std::istream& f);
+
+		friend std::ostream& operator << (std::ostream &f, Debug &t){
+			t.print(f);
+			return f;
+		};
+		friend std::istream& operator >> (std::istream &f, Debug &t){
+			std::cout << "JE MARCHE" << std::endl;
+			t.read(f);
+			std::cout << "JE MARCHE ENCORE" << std::endl;
+			return f;
+		};
 };
 
 #endif
