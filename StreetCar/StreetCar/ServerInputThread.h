a53efd43b2967508c60a9bb084d4e81ec92a9060
jeremie finiel <jeremiefiniel@gmail.com>
2015-05-27 14:57:31 +0200

    implementation des thread réseau

--- StreetCar/StreetCar/ServerInputThread.h
+++ StreetCar/StreetCar/ServerInputThread.h
@@ -2,16 +2,32 @@
 #define SERVERINPUTTHREAD_H
 
 #include <QThread>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 class ServerInputThread : public QThread
 {
 		Q_OBJECT
 	public:
 		explicit ServerInputThread(QObject *parent = 0);
+		ServerInputThread(int socket);
 
 	signals:
 
 	public slots:
+
+	private:
+		void run();
+
+		int sockfd;
 
 };
 
