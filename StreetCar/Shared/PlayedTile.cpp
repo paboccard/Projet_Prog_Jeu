0ed3a6a7e4c76370783e42d8b2220f3209923850
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-27 17:23:11 +0200

    modifié:         ../Shared/PlayedTile.cpp 	modifié:         ../Shared/PlayedTravel.cpp

--- StreetCar/Shared/PlayedTile.cpp
+++ StreetCar/Shared/PlayedTile.cpp
@@ -4,15 +4,13 @@ using namespace std;
 
 PlayedTile::PlayedTile() : Pack(){}
 
-PlayedTile::PlayedTile(int idNextP, vector<Tile> tilesPlayed): Pack(){
+PlayedTile::PlayedTile(vector<Tile> tilesPlayed): Pack(){
     idPack = PLAYEDTILE;
-    idNextPlayer = idNextP;
     tiles = tilesPlayed;
 }
 
 void PlayedTile::print(std::ostream& f){
     f << PLAYEDTILE << " ";
-    f << idNextPlayer << " ";
     f << tiles.size() << " ";
     for (unsigned int i = 0; i<tiles.size(); i++)
 	f << tiles[i] << " ";
@@ -22,7 +20,6 @@ void PlayedTile::read(std::istream& f){
     int idP;
     f >> idP;
     idPack = PLAYEDTILE;
-    f >> idNextPlayer;
     int nbInVectorTiles;
     f >> nbInVectorTiles;
     tiles.clear();
--- StreetCar/Shared/PlayedTravel.cpp
+++ StreetCar/Shared/PlayedTravel.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 PlayedTravel::PlayedTravel() : Pack(){}
 
-PlayedTravel::PlayedTravel(Travel travelOfTram): Pack(){
+PlayedTravel::PlayedTravel(int idNextP, Travel travelOfTram): Pack(){
     idPack = PLAYEDTRAVEL;
+    idNextPlayer = idNextP;
     tram = travelOfTram;
 }
 
 void PlayedTravel::print(ostream& f){
     f << PLAYEDTRAVEL << " ";
+    f << idNextPlayer << " ";
     f << tram;
 }
 
@@ -18,6 +20,7 @@ void PlayedTravel::read(istream& f){
     int i;
     f >> i;
     idPack = PLAYTRAVEL;
+    f >> idNextPlayer;
     Travel travel;
     f >> travel;
     tram = travel;
