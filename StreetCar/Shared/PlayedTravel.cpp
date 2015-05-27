870c0f79afadff22438af61a50fa0b1c088ea863
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-27 17:03:49 +0200

    modifié:         ../Shared/PlayedTravel.cpp

--- StreetCar/Shared/PlayedTravel.cpp
+++ StreetCar/Shared/PlayedTravel.cpp
@@ -4,23 +4,20 @@ using namespace std;
 
 PlayedTravel::PlayedTravel() : Pack(){}
 
-PlayedTravel::PlayedTravel(int idNextP, Travel travelOfTram): Pack(){
+PlayedTravel::PlayedTravel(Travel travelOfTram): Pack(){
     idPack = PLAYEDTRAVEL;
-    idNextPlayer = idNextP;
     tram = travelOfTram;
 }
 
 void PlayedTravel::print(ostream& f){
     f << PLAYEDTRAVEL << " ";
-    f << idNextPlayer << " ";
     f << tram;
 }
 
 void PlayedTravel::read(istream& f){
-    int idP;
-    f >> idP;
+    int i;
+    f >> i;
     idPack = PLAYTRAVEL;
-    f >> idNextPlayer;
     Travel travel;
     f >> travel;
     tram = travel;
