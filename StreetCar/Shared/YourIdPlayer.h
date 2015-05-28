8cf678019374b53fddf13c9e3070fef6cf92eb0f
Pierre-Alexis <pa.boccard@gmail.com>
2015-05-28 16:46:55 +0200

    add class YourIdPlayer

---
+++ StreetCar/Shared/YourIdPlayer.h
@@ -0,0 +1,30 @@
+#ifndef YOURIDPLAYER_H
+#define YOURIDPLAYER_H
+
+#include "Pack.h"
+#include <iostream>
+#include <fstream>
+
+class YourIdPlayer : public Pack{
+    
+ public:
+    
+    int nbrPlayer;
+    
+    YourIdPlayer();
+    YourIdPlayer(int nbrP);
+
+    void print(std::ostream& f);
+    void read(std::istream& f);
+
+    friend std::ostream& operator << (std::ostream &f, YourIdPlayer &t){
+	t.print(f);
+	return f;
+    };
+    friend std::istream& operator >> (std::istream &f, YourIdPlayer &t){
+	t.read(f);
+	return f;
+    };
+};
+
+#endif
