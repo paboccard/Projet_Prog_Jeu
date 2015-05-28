f036435d6f6b1c956e807526643380420e78c4ba
jeremie finiel <jeremiefiniel@gmail.com>
2015-05-28 16:28:35 +0200

    modification dans l'affectation des point

--- StreetCar/Shared/Point.h
+++ StreetCar/Shared/Point.h
@@ -5,12 +5,16 @@
 struct Point{
     int x;
     int y;
+	Point operator =(Point p){
+		x = p.x;
+		y = p.y;
+		return p;
+	}
 };
 
 
 typedef struct Point Point;
 
-void operator != (Point &p1, Point &p2);
 bool operator != (Point &p1, Point &p2);
 bool operator == (Point &p1, Point &p2);
 
