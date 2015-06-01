bf0e3c5a1b7d296382dcc4c81fd42e0594fa115b
jeremie finiel <jeremiefiniel@gmail.com>
2015-06-01 08:19:13 +0200

    Modification dans le teste de remplacement

--- StreetCar/Shared/Tile.cpp
+++ StreetCar/Shared/Tile.cpp
@@ -974,24 +974,30 @@ Tile::Tile(idTile t, int p, int x, int y) : Square(t, x, y) {
 }
 
 bool Tile::change(Tile t){
-    if(tree)
-	return false;
-    switch (t.type){
-    case Straight:
-	return type==Intersect || type==StraightLCurve || type==StraightRCurve || type==HStraightVCurve || type==VStraightVCurve || type==StraightLDoubleCurves || type==StraightRDoubleCurves;
-    case Curve:
-	return !(type==Intersect);
-    case StraightRCurve:
-	return type==StraightRDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
-    case StraightLCurve:
-	return type==StraightLDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
-    case VCurve:
-	return type==HStraightVCurve || type==VStraightVCurve || type==CrossCurves;
-    case DoubleCurves:
-	return type==StraightLDoubleCurves || type==StraightRDoubleCurves || type==CrossCurves;
-    default:
-	return false;
-    }
+	if(!isTile() || tree)
+		return false;
+
+	for (int i = WEST; i <= NORTH; i ++) {
+		if (access[i] == OBLIGATORY && t[i] != OBLIGATORY)
+			return false;
+	}
+	return true;
+	/*switch (t.type){
+		case Straight:
+			return type==Intersect || type==StraightLCurve || type==StraightRCurve || type==HStraightVCurve || type==VStraightVCurve || type==StraightLDoubleCurves || type==StraightRDoubleCurves;
+		case Curve:
+			return !(type==Intersect);
+		case StraightRCurve:
+			return type==StraightRDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
+		case StraightLCurve:
+			return type==StraightLDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
+		case VCurve:
+			return type==HStraightVCurve || type==VStraightVCurve || type==CrossCurves;
+		case DoubleCurves:
+			return type==StraightLDoubleCurves || type==StraightRDoubleCurves || type==CrossCurves;
+		default:
+			return false;
+	}*/
 }
 
 void Tile::rotate(){
