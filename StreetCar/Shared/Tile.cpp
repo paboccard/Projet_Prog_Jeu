c8e75e142411c6addac28d43101c6d2e95c05547
jeremie finiel <jeremiefiniel@gmail.com>
2015-06-01 09:48:56 +0200

    Modification dans la verification du changement de tuile

--- StreetCar/Shared/Tile.cpp
+++ StreetCar/Shared/Tile.cpp
@@ -971,24 +971,31 @@ Tile::Tile(idTile t, int p, int x, int y) : Square(t, x, y) {
 }
 
 bool Tile::change(Tile t){
-    if(tree)
-	return false;
-    switch (t.type){
-    case Straight:
-	return type==Intersect || type==StraightLCurve || type==StraightRCurve || type==HStraightVCurve || type==VStraightVCurve || type==StraightLDoubleCurves || type==StraightRDoubleCurves;
-    case Curve:
-	return !(type==Intersect);
-    case StraightRCurve:
-	return type==StraightRDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
-    case StraightLCurve:
-	return type==StraightLDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
-    case VCurve:
-	return type==HStraightVCurve || type==VStraightVCurve || type==CrossCurves;
-    case DoubleCurves:
-	return type==StraightLDoubleCurves || type==StraightRDoubleCurves || type==CrossCurves;
-    default:
-	return false;
-    }
+	if(!isTile() || tree || type == t.type)
+		return false;
+
+	for (int i = WEST; i <= NORTH; i ++)
+		if (access[i] == OBLIGATORY && t.access[i] != OBLIGATORY)
+			return false;
+
+	return true;
+	/*
+	switch (t.type){
+		case Straight:
+			return type==Intersect || type==StraightLCurve || type==StraightRCurve || type==HStraightVCurve || type==VStraightVCurve || type==StraightLDoubleCurves || type==StraightRDoubleCurves;
+		case Curve:
+			return !(type==Intersect);
+		case StraightRCurve:
+			return type==StraightRDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
+		case StraightLCurve:
+			return type==StraightLDoubleCurves || type==HStraightVCurve || type==VStraightVCurve;
+		case VCurve:
+			return type==HStraightVCurve || type==VStraightVCurve || type==CrossCurves;
+		case DoubleCurves:
+			return type==StraightLDoubleCurves || type==StraightRDoubleCurves || type==CrossCurves;
+		default:
+			return false;
+	}*/
 }
 
 void Tile::rotate(){
