e47ad124dfb2e2a4fe44d5673f3fbdd70fd589cd
lefrerej <lefrerej@F202-06.e-im2ag.ujf-grenoble.fr>
2015-06-01 17:06:46 +0200

    creation of the class Card

--- StreetCar/Shared/Card.cpp
+++ StreetCar/Shared/Card.cpp
@@ -1,10 +1,66 @@
 #include "Card.h"
 
 
-Card::Card() {}
+Card::Card(int card) {
+    switch (card){
+    case 0:
+	add(0,StationC,StationD,StationI);
+	add(1,StationB,StationD,StationM);
+	add(2,StationG,StationK,StationL);
+	add(3,StationE,StationF,StationK);
+	add(4,StationE,StationH,StationK);
+	add(5,StationA,StationL,StationM);
+	break;
+    case 1:
+	add(0,StationF,StationH,StationK);
+	add(1,StationC,StationF,StationI);
+	add(2,StationG,StationM,StationL);
+	add(3,StationB,StationH,StationL);
+	add(4,StationD,StationI,StationM);
+	add(5,StationB,StationF,StationI);
+	break;
+    case 2:
+	add(0,StationB,StationG,StationL);
+	add(1,StationB,StationL,StationM);
+	add(2,StationC,StationI,StationM);
+	add(3,StationA,StationD,StationM);
+	add(4,StationA,StationG,StationK);
+	add(5,StationB,StationF,StationM);
+	break;
+    case 3:
+	add(0,StationF,StationI,StationK);
+	add(1,StationE,StationG,StationI);
+	add(2,StationD,StationH,StationK);
+	add(3,StationH,StationK,StationL);
+	add(4,StationA,StationE,StationL);
+	add(5,StationA,StationB,StationL);
+	break;
+    case 4:
+	add(0,StationA,StationC,StationL);
+	add(1,StationC,StationG,StationK);
+	add(2,StationD,StationH,StationI);
+	add(3,StationC,StationE,StationM);
+	add(4,StationA,StationB,StationM);
+	add(5,StationE,StationI,StationK);
+	break;
+    case 5:
+	add(0,StationC,StationG,StationM);
+	add(1,StationG,StationH,StationL);
+	add(2,StationC,StationD,StationM);
+	add(3,StationA,StationE,StationI);
+	add(4,StationD,StationF,StationI);
+	add(5,StationE,StationK,StationL);
+	break;
 
-Card::Card(int line, int station1, int station2, int station3){
-	card[line][0] = station1;
-	card[line][1] = station2;
-	card[line][2] = station3;
+    }
 }
+vector<idTile> Card::whichStation(int line){
+    return {card[line][0],card[line][1],card[line][2]};
+}
+
+Card::add(int line, idTile station1, idTile station2, idTile station3){
+    card[line][0] = station1;
+    card[line][1] = station2;
+    card[line][2] = station3;
+}
+
