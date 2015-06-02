fd8ccd8a67fe13803c212538c9c56cd8c5d8d24c
Pierre-Alexis <pa.boccard@gmail.com>
2015-06-02 19:52:46 +0200

    bug fix

--- StreetCar/Computer/ApplicationComputer.cpp
+++ StreetCar/Computer/ApplicationComputer.cpp
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]){
     //Adress by IP
     serv_addr.sin_addr.s_addr = inet_addr("152.77.82.244");
     //bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
+ 
     cout << "start to connect to the server " << sockfd << endl;
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 	cout << "ERROR connecting " << endl;
@@ -82,6 +82,7 @@ int main(int argc, char *argv[]){
     }else
 	cout << "ERROR, impossible to create server " << endl;
 
+   
     while (idPlayer == -1){
 	Profile profile = Profile((string)argv[1],atoi(argv[2]));
 	IWantPlay *p = new IWantPlay(profile);
@@ -123,23 +124,23 @@ int main(int argc, char *argv[]){
 	    switch(readPack->idPack){
 	    case PLAYEDTILE:
 		{
+		    //modif board
 		    PlayedTile *pt = (PlayedTile*)readPack;
 		    for (int i = 0; i<NBR_TILE_MAX; i++){
 			computer->getBoard()->change(pt->tiles[i]);
 			computer->getPlayers(currentPlayer)->setHand(pt->tiles[i],pt->idxTiles[i]);
 		    }
-		    //TODO modif board
 		}
 		break;
 	    case PILEPLAYER:
 		{
+		    //modif pile & hand last player
 		    PilePlayer* pp = (PilePlayer*)readPack;
 		    for (unsigned int i = 0; i < pp->tilesPiled.size(); i++){
 			computer->getPlayers(pp->idPlayer)->setHand(&pp->tilesPiled[i],pp->idxTiles[i]);
 			computer->setPile((int)pp->tilesPiled[i].getType());
 		    }
 		    currentPlayer = pp->idNextPlayer;
-		    //TODO modif pile & hand last player
 		}
 		break;
 	    default:
