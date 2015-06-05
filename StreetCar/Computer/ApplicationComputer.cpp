b5db9dc65fad4fa51b849a7f70f435659558b136
Pierre-Alexis <pa.boccard@gmail.com>
2015-06-05 10:51:40 +0200

    bug fix

--- StreetCar/Computer/ApplicationComputer.cpp
+++ StreetCar/Computer/ApplicationComputer.cpp
@@ -85,7 +85,7 @@ int main(int argc, char *argv[]){
 
    
     while (idPlayer == -1){
-	Profile profile = Profile((string)argv[1],atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
+	Profile profile = Profile((string)argv[1],atoi(argv[2]),atoi(argv[4]));
 	IWantPlay *p = new IWantPlay(profile);
 	prodConsOutput->produce(p);
 	readPack = prodConsInput->consume();
