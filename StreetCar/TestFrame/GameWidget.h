944c97c21da05a4710c24d390a33e9cb0b8deb86
jeremie finiel <jeremiefiniel@gmail.com>
2015-06-05 13:38:20 +0200

    bug fix

--- StreetCar/TestFrame/GameWidget.h
+++ StreetCar/TestFrame/GameWidget.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QDrag>
+#include "PlayerWidget.h"
 #include "HandWidget.h"
 #include "BoardView.h"
 #include "../Shared/Player.h"
@@ -22,12 +23,17 @@ class GameWidget : public QWidget
 		BoardView *getBoard();
 
 	public slots:
+		void tileDrop(int);
+		void tileChange(int, Tile);
 
 	protected:
 		void mousePressEvent(QMouseEvent *e);
+
 	private:
+		QHBoxLayout *layoutPlayer;
 		int currentPlayer;
 		int currentId;
+		PlayerWidget *playerWidget[];
 		QVector<Player*> players;
 		BoardView *board;
 		HandWidget *hand;
