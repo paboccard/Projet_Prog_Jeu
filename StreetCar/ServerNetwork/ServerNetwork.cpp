e71f5a6898c99fa5efc603b166244959bb30d1a5
Pierre-Alexis <pa.boccard@gmail.com>
2015-06-09 10:27:15 +0200

    modifié:         ServerNetwork.cpp

--- StreetCar/ServerNetwork/ServerNetwork.cpp
+++ StreetCar/ServerNetwork/ServerNetwork.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]){
 		  CreateGameNetwork *c = (CreateGameNetwork*)readPack;
 		  
 		  gameNetwork.push_back((GameNetwork){c->gameNetwork.name, c->gameNetwork.nbrPlayers});
-		  playersConnected.push_back(prodConsServer);
+		  playersConnected.push_back(c->prodConsClient);
 		  for (unsigned int i = 0; i< playersConnected.size() ; i++)
 		      playersConnected[i]->produce(new ResponseRefresh(gameNetwork));
 
